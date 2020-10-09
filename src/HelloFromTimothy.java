da67502c0972a696aa7ee2292487cb25b2806482
Megan De Leon <70547927+MeganDLS@users.noreply.github.com>
2020-10-09 13:55:19 -0600

    Update HelloFromTimothy.java

--- src/HelloFromTimothy.java
+++ src/HelloFromTimothy.java
@@ -1,5 +1,6 @@
 public class HelloFromTimothy {
     public static void main(String[] args) {
         System.out.println("Hello from Timothy.");
+        System.out.println("Hello Timothy, this is Megan.");
     }
 }
